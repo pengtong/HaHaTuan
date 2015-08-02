1692e88ce6baf3d8a625b078dfd78658dc02a1ff
Pengtong <user@UserdeMacBook-Pro.local>
2015-08-02 13:27:42 +0800

    Initial Commit

---
+++ HaHaMeituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaHaMeituan
+//
+//  Created by Pengtong on 15/8/2.
+//  Copyright (c) 2015年 Pengtong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
